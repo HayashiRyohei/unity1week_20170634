05716bc889a87c02bcf2b219a282c7e3f91ad93c
RyoheiHayashi <c06955960c@yahoo.co.jp>
2017-06-24 22:12:56 +0900

    ・chagne resource

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -1,24 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 public class Food : MonoBehaviour {
 
-	Action collisionDel = null;
 
 	#region mono
 	void OnCollisionEnter(Collision coll) {
-		if (collisionDel != null) {
-			collisionDel ();
+		if (coll.gameObject.tag == "Respawn") {
+			this.gameObject.tag = "Respawn";
+			this.gameObject.transform.SetParent (coll.gameObject.transform.parent);
+		}
+
+		if (coll.gameObject.tag == "Finish") {
+			Destroy (this.gameObject);
 		}
 	}
 	#endregion
-
-	#region public function
-	public void Init(Action collDel) {
-		collisionDel = collDel;
-	}
-	#endregion
-
 }
--- Assets/Scripts/ObjectShotter.cs
+++ Assets/Scripts/ObjectShotter.cs
@@ -6,10 +6,8 @@ public class ObjectShotter : MonoBehaviour {
 
 	[SerializeField]
 	float power = 1;
-	[SerializeField]
-	Vector3 direction = new Vector3(0, 0, 0);
-	[SerializeField]
-	GameObject obj;
+
+	private Vector3 direction = new Vector3(0, -1, 0);
 	List<GameObject> foodList = new List<GameObject>();
 
 	[SerializeField]
@@ -17,7 +15,7 @@ public class ObjectShotter : MonoBehaviour {
 
 	#region mono
 	void Update() {
-		if (Input.GetKeyDown(KeyCode.Return)) {
+		if (Input.GetKeyDown(KeyCode.Space)) {
 			Shot ();
 		}
 
@@ -34,17 +32,10 @@ public class ObjectShotter : MonoBehaviour {
 	private void Shot() {
 		if (foodList.Count > 0) {
 			GameObject _obj = (GameObject)Instantiate (foodList [0]);
-			_obj.GetComponent<Food> ().Init (CollisionEnter);
+			_obj.transform.position = this.gameObject.transform.position;
 			_obj.GetComponent<Rigidbody> ().AddForce (direction * power, ForceMode.Impulse);
 			foodList.RemoveAt (0);
 		}
-	}
-
-	/// <summary>
-	/// 飛ばした食材がぶつかったときに呼ばれるコールバック
-	/// </summary>
-	private void CollisionEnter() {
-		Debug.Log ("test");
 	}
 	#endregion
 
